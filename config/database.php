90126b9197013c9eead5d1ba4b1e2488a8dcc1af
杨可可 <kecoyo@163.com>
2021-01-24 16:10:15 +0800

    数据库更换；

--- config/database.php
+++ config/database.php
@@ -30,7 +30,7 @@ return [
             // 服务器地址
             'hostname'        => 'mysql',
             // 数据库名
-            'database'        => 'admin_v6',
+            'database'        => 'turtle-admin',
             // 用户名
             'username'        => 'root',
             // 密码
