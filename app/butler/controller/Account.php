d401b6c02c2978e560785b44f10badcf83964cf5
杨可可 <kecoyo@163.com>
2021-01-30 07:25:36 +0800

    账号管理添加账号移动到功能；

--- app/butler/controller/Account.php
+++ app/butler/controller/Account.php
@@ -77,6 +77,18 @@ class Account extends Controller
     }
 
     /**
+     * 移动账号
+     * @auth true
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function move()
+    {
+        $this->_form($this->table, 'move');
+    }
+
+    /**
      * 表单数据处理
      * @param array $data
      * @throws \think\db\exception\DataNotFoundException
@@ -85,11 +97,37 @@ class Account extends Controller
      */
     protected function _form_filter(array &$data)
     {
-        if ($this->request->isGet()) {
-            if (!isset($data['id'])) {
-                $data['category_id'] = intval($data['category_id'] ?? input('category_id', '0'));
-                $data['icon'] = '//cdn.kecoyo.com/upload/butler_icon/60/73c09bbc4f2b3c0bbef121c216bb96.png';
-                $data['properties'] = '[{"name":"账号","value":"kecoyo"}]';
+        if ($this->request->action() === 'move') {
+            if ($this->request->isGet()) {
+                $this->category_id = input('category_id', '0');
+                $this->account_ids = input('id', '');
+                $this->clist = $this->app->db->name('ButlerCategory')->where(['is_deleted' => 0, 'status' => 1])->order('sort desc,id desc')->select()->toArray();
+            } else {
+                $category_id = input('category_id', '0');
+                $account_ids_str = input('account_ids', '0');
+
+                // 图标保存
+                $account_ids = explode(',', $account_ids_str);
+                $result = true;
+
+                $result = $this->app->db->name('ButlerAccount')->where('id', 'in', $account_ids)->update([
+                    'category_id'  => $category_id,
+                ]);
+
+                if ($result !== false) {
+                    $this->success(lang('think_library_form_success'), 'javascript:history.back()');
+                } else {
+                    $this->error(lang('think_library_form_error'));
+                }
+                return false;
+            }
+        } else {
+            if ($this->request->isGet()) {
+                if (!isset($data['id'])) {
+                    $data['category_id'] = intval($data['category_id'] ?? input('category_id', '0'));
+                    $data['icon'] = '//cdn.kecoyo.com/upload/butler_icon/60/73c09bbc4f2b3c0bbef121c216bb96.png';
+                    $data['properties'] = '[{"name":"账号","value":"kecoyo"}]';
+                }
             }
         }
     }
