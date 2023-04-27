cd519c4c6f257c49d044a65962f1457be9d500e9
杨可可 <kecoyo@163.com>
2023-04-27 15:14:02 +0800

    模块IconType完成

--- app/butler/controller/IconType.php
+++ app/butler/controller/IconType.php
@@ -2,8 +2,9 @@
 
 namespace app\butler\controller;
 
+use app\butler\model\ButlerIconType;
 use think\admin\Controller;
-use think\admin\extend\DataExtend;
+use think\admin\helper\QueryHelper;
 
 /**
  * 图标分类管理
@@ -12,12 +13,6 @@ use think\admin\extend\DataExtend;
  */
 class IconType extends Controller
 {
-    /**
-     * 绑定数据表
-     * @var string
-     */
-    private $table = 'ButlerIconType';
-
     /**
      * 图标分类管理
      * @auth true
@@ -28,91 +23,51 @@ class IconType extends Controller
      */
     public function index()
     {
-        $this->title = "图标分类管理";
-        $query = $this->_query($this->table)->like('name')->dateBetween('create_at');
-        $query->equal('status')->where(['is_deleted' => 0])->order('sort asc,id desc')->page(false);
+        ButlerIconType::mQuery()->layTable(function () {
+            $this->title = '图标分类管理';
+        }, function (QueryHelper $query) {
+            $query->where(['is_deleted' => 0]);
+            $query->like('name,remark')->equal('status')->dateBetween('create_at')->order('sort asc,id desc');
+        });
     }
 
     /**
      * 添加图标分类
      * @auth true
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
      */
     public function add()
     {
-        $this->_applyFormToken();
-        $this->_form($this->table, 'form');
+        ButlerIconType::mForm('form');
     }
 
     /**
      * 编辑图标分类
      * @auth true
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
      */
     public function edit()
     {
-        $this->_applyFormToken();
-        $this->_form($this->table, 'form');
-    }
-
-    /**
-     * 删除图标分类
-     * @auth true
-     * @throws \think\db\exception\DbException
-     */
-    public function remove()
-    {
-        $this->_delete($this->table);
+        ButlerIconType::mForm('form');
     }
 
     /**
      * 修改图标分类状态
      * @auth true
-     * @throws \think\db\exception\DbException
      */
     public function state()
     {
-        $this->_save($this->table, $this->_vali([
+        ButlerIconType::mSave($this->_vali([
             'status.in:0,1'  => '状态值范围异常！',
             'status.require' => '状态值不能为空！',
         ]));
     }
 
     /**
-     * 修改图标分类顺序
+     * 删除图标分类
      * @auth true
-     * @throws \think\db\exception\DbException
      */
-    public function sort()
+    public function remove()
     {
-        $id = intval($this->app->request->post('id', 0));
-        $sort = intval($this->app->request->post('sort', 0));
-
-        if (!$id || !$sort) {
-            $this->error(lang('think_library_sort_error'));
-        }
-
-        // 账号列表
-        $list = $this->app->db->name($this->table)->where(['is_deleted' => 0])->order('sort asc,id desc')->select()->toArray();
-
-        // 查找原位置
-        $index = array_search($id, array_column($list, 'id'));
-
-        // 数组排序
-        array_splice($list, $sort - 1, 0, array_splice($list, $index, 1));
-
-        // 修改有变化排序号
-        foreach ($list as $key => $vo) {
-            $new_sort = $key + 1;
-            if ($new_sort !== $vo['sort']) {
-                $this->app->db->name($this->table)->where(['id' => $vo['id']])->update(['sort' => $new_sort]);
-            }
-        }
-
-        $this->success(lang('think_library_sort_success'));
+        ButlerIconType::mDelete();
     }
+
 }
